033503a676828c68388adcc9cffd5a28231873ee
赵洋 <zhaoyang03@ppdai.com>
2018-07-17 13:13:19 +0800

    init

---
+++ StockLottery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StockLottery
+//
+//  Created by John on 2018/7/17.
+//  Copyright © 2018年 jiangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StockLottery/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  StockLottery
+//
+//  Created by John on 2018/7/17.
+//  Copyright © 2018年 jiangyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) UIView *displayView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIView *displayView = [[UIView alloc] init];
+    displayView.backgroundColor = [UIColor redColor];
+    displayView.frame = CGRectMake(0, 0, 100, 100);
+    displayView.center = self.view.center;
+    [self.view addSubview:displayView];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
